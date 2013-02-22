857c6fcefb3d6ddb44b46c96596b293df7033863
Mike Carr <mcarr@qualcomm.com>
2013-02-21 16:10:01 -0800

    latest changes

--- connect.sh
+++ connect.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -classpath "lib/ojdbc6_g.jar:build/jar/*" JdbcConnect
+java -Djava.util.logging.config.file=oracle-log.properties -Doracle.jdbc.Trace=true -classpath "lib/ojdbc6_g.jar:build/jar/*" JdbcConnect
