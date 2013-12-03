fcd4f4849f17e8371a70bee8408ac08bda0e561d
mcarr <T0mmyismyson>
2013-12-03 13:33:04 -0800

    mode dynamic

--- connect.sh
+++ connect.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+#java -Djava.util.logging.config.file=conf/logger.properties \
+#    -Doracle.jdbc.Trace=true \
+#    -classpath "lib/ojdbc6_g.jar:build/classes" JdbcConnect
+
 java -Djava.util.logging.config.file=conf/logger.properties \
     -Doracle.jdbc.Trace=true \
-    -classpath "lib/ojdbc6_g.jar:build/classes" JdbcConnect
+    -cp "lib/*:build/classes" JdbcConnect
--- src/JdbcConnect.java
+++ src/JdbcConnect.java
@@ -2,9 +2,7 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 import java.util.logging.Logger;
 
@@ -28,13 +26,23 @@ public class JdbcConnect{
             String url = prop.getProperty("url");
             String username = prop.getProperty("username");
             String password = prop.getProperty("password");
+            String classname = prop.getProperty("classname");
+            String query = prop.getProperty("query");
 
-            Class.forName("oracle.jdbc.driver.OracleDriver");
-
-
+            //Class.forName("oracle.jdbc.driver.OracleDriver");
+            Class.forName(classname);
 
             Connection conn = DriverManager.getConnection(url, username, password);
             System.out.println("Connected!");
+
+            Statement stmt = conn.createStatement();
+            logger.info(" jdbc-connect is creating query: '" + query + "'!");
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                System.out.println(rs.getString(1));
+            }
+
+            System.out.println("Done!");
 
 
         } catch (ClassNotFoundException e) {
